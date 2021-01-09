b51d26bed01bf434ce2a4e1dc16aa2113a580149
Milind-Satpute <34695039+Milind-Satpute@users.noreply.github.com>
2021-01-09 19:44:01 +0000

    Updated API CODE and HTML UI

--- AnalysisEngine/AnalysisAPI.py
+++ AnalysisEngine/AnalysisAPI.py
@@ -1,24 +1,30 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import json
-from Engine import getTotal,getAnalysisData
-app = Flask(__name__)
-#https://stackoverflow.com/questions/13081532/return-json-response-from-flask-view
-@app.route('/GetData')
-def GetData():
-   val=getAnalysisData();
-   df_list = val.values.tolist()
-   JSONP_data = jsonify(df_list)
-   return JSONP_data
+from Engine import getTotal, getAnalysisData
 
+app = Flask(__name__)
+CORS(app)
+
+
+# https://stackoverflow.com/questions/13081532/return-json-response-from-flask-view
+@app.route('/GetData')
+def GetData(data):
+    print(data);
+    val = getAnalysisData(data);
+    df_list = val.values.tolist()
+    JSONP_data = jsonify(df_list)
+    return JSONP_data
 
 
 @app.route('/GetTotal')
 def GetTotal():
     val = getAnalysisData();
-    total=getTotal(val);
+    total = getTotal(val);
     df_list = total.values.tolist()
     JSONP_data = jsonify(df_list)
     return JSONP_data
 
+
 if __name__ == '__main__':
-   app.run()
+    app.run()
